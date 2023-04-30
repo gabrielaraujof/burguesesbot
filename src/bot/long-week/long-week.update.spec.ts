5bbff3754679be90bbfadd3f431bb17dec79eed4
Gabriel Araujo <gabrielferreira@ciandt.com>
2023-04-30 13:07:28 -0300

    test: add missing unit tests

--- src/bot/long-week/long-week.update.spec.ts
+++ src/bot/long-week/long-week.update.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LongWeekUpdate } from './long-week.update';
+import { Context } from 'telegraf';
+import { Message, Update } from 'typegram';
 
 describe('LongWeekService', () => {
   let service: LongWeekUpdate;
@@ -14,5 +16,33 @@ describe('LongWeekService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should reply with "cadê a live?"', async () => {
+    const ctx = {
+      reply: jest.fn(),
+    } as unknown as Context<Update.MessageUpdate<Message.TextMessage>>;
+
+    await service.LongWeekReply(ctx);
+
+    expect(ctx.reply).toBeCalledWith('cadê a live?');
+  });
+
+  it('should reply with "Foda-se." and the user name', async () => {
+    const ctx = {
+      message: {
+        from: {
+          first_name: 'John',
+          last_name: 'Doe',
+        },
+      },
+      replyWithMarkdownV2: jest.fn(),
+    } as unknown as Context<Update.MessageUpdate<Message.TextMessage>>;
+
+    await service.fuck(ctx);
+
+    expect(ctx.replyWithMarkdownV2).toBeCalledWith(
+      'Foda\\-se\\.\n\\- _Doe, John_',
+    );
   });
 });
--- src/bot/notify/notify.service.spec.ts
+++ src/bot/notify/notify.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotifyService } from './notify.service';
+import { Context, Telegraf } from 'telegraf';
+import { DEFAULT_BOT_NAME } from 'nestjs-telegraf';
 
-describe('NotifyService', () => {
+describe('Notify Service', () => {
   let service: NotifyService;
+  let bot: Telegraf<Context>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotifyService],
+      providers: [
+        NotifyService,
+        {
+          provide: DEFAULT_BOT_NAME,
+          useValue: {
+            telegram: {
+              sendMessage: jest.fn(),
+            },
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<NotifyService>(NotifyService);
+    bot = module.get<Telegraf<Context>>(DEFAULT_BOT_NAME);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should call notify method', async () => {
+    const chatId = '123';
+    const message = 'Hello World';
+
+    service.notify(chatId, message);
+
+    expect(bot.telegram.sendMessage).toBeCalledWith(chatId, message);
   });
 });
