e308fd5cb18c4701a2209e25aa40f51b9eb045d4
Gabriel Araujo <git@gabrielaraujo.dev>
2024-06-14 13:11:47 -0300

    test: handler invocation

--- src/function.ts
+++ src/function.ts
@@ -1,9 +1,11 @@
-import http from 'serverless-http';
+import http from 'serverless-http'
 
-import createBot from './bot.js';
+import createBot from './bot.js'
 
-const bot = createBot(process.env.BOT_TOKEN ?? '');
+const bot = createBot(process.env.BOT_TOKEN ?? '')
 
 console.log('Creating bot', process.env.BOT_TOKEN, process.env.WEBHOOK_SECRET_PATH)
 
-export const handler = http(bot.webhookCallback(process.env.WEBHOOK_SECRET_PATH));
+export const handler = (ctx: any) => {
+    console.log('Starting', ctx)
+}
