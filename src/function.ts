480158a5592630c668bfe6c9140b71b08f3ffc76
Gabriel Araujo <git@gabrielaraujo.dev>
2024-06-14 16:52:35 -0300

    add: serverless-express as a proxy

--- src/function.ts
+++ src/function.ts
@@ -1,9 +1,24 @@
-import http from 'serverless-http'
+import type { Callback, Context, Handler } from 'aws-lambda';
+import { configure } from '@codegenie/serverless-express';
 
-import createBot from './bot.js'
+import createBot from './bot.js';
 
-const bot = createBot(process.env.BOT_TOKEN ?? '')
+let proxy: Handler;
 
-console.log(`Creating bot on ${process.env.WEBHOOK_SECRET_PATH}`)
+async function setup(event: any, context: Context, cb: Callback) {
+  const bot = createBot(process.env.BOT_TOKEN ?? '');
+  console.log(`Creating bot on ${process.env.WEBHOOK_SECRET_PATH}`);
 
-export const handler = http(bot.webhookCallback(process.env.WEBHOOK_SECRET_PATH))
+  const app = bot.webhookCallback(process.env.WEBHOOK_SECRET_PATH);
+
+  proxy = configure({ app });
+  return proxy(event, context, cb);
+}
+
+function handler(event: any, context: Context, cb: Callback) {
+  if (proxy) return proxy(event, context, cb);
+
+  return setup(event, context, cb);
+}
+
+export { handler };
