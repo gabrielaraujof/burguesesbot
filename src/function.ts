6612dbd9f329ae2c8e8dbb47c5fecbaaa4eb2082
Gabriel Araujo <git@gabrielaraujo.dev>
2024-06-14 14:30:48 -0300

    test: strict path

--- src/function.ts
+++ src/function.ts
@@ -4,8 +4,6 @@ import createBot from './bot.js'
 
 const bot = createBot(process.env.BOT_TOKEN ?? '')
 
-console.log('Creating bot', process.env.BOT_TOKEN, process.env.WEBHOOK_SECRET_PATH)
+console.log(`Creating bot on ${process.env.WEBHOOK_SECRET_PATH}`)
 
-export const handler = (ctx: any) => {
-    console.log('Starting', ctx)
-}
+export const handler = http(bot.webhookCallback(process.env.WEBHOOK_SECRET_PATH))
