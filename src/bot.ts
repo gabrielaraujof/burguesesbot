2433493361c76f160e568b06fa0c4d042fe27b26
Gabriel Araujo <git@gabrielaraujo.dev>
2024-06-14 01:40:17 -0300

    add: simple logging

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,11 @@
 import { Telegraf } from 'telegraf'
 
 export default (token: string) => {
+    console.log('Starting bot')
     const bot = new Telegraf(token)
-    bot.command('semanalonga', (ctx) => ctx.reply('cadê a live?'))
+    bot.command('semanalonga', async (ctx) => {
+        console.log('Answering semanalonga')
+        await ctx.reply('cadê a live?')
+    })
     return bot
 }
--- src/function.ts
+++ src/function.ts
@@ -4,4 +4,6 @@ import createBot from './bot.js';
 
 const bot = createBot(process.env.BOT_TOKEN ?? '');
 
+console.log('Creating bot', process.env.BOT_TOKEN, process.env.WEBHOOK_SECRET_PATH)
+
 export const handler = http(bot.webhookCallback(process.env.WEBHOOK_SECRET_PATH));
